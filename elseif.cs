af6104e5aba4ccda1c0e10f619f3999dcb71c0aa
swetha_suvi <80690771+suvimanikandan@users.noreply.github.com>
2023-11-20 16:45:32 +0530

    Create elseif.cs

---
+++ elseif.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    bool isMale = true;
+    bool isTall = false;
+    
+    if(isMale && isTall)
+    {
+    Console.WriteLine("you are a tall male");
+    }
+    else if(isMale && !isTall) {
+    Console.WriteLine("you are a short male");
+    }
+    else{
+    
+    Console.WriteLine("You are either not male or not tall or both");
+    }
+Console.ReadLine();
+}
+}
+}
