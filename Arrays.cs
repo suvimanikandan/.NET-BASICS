92253f718a28d2727b58a56f8c6d1df740e1b648
swetha_suvi <80690771+suvimanikandan@users.noreply.github.com>
2023-11-20 15:51:58 +0530

    Create Arrays.cs

---
+++ Arrays.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");    
+          
+          int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+          
+          String [] friends = new string[5];
+          friends[0]="Jim";
+          friends[1]="kelly";
+            Console.WriteLine(luckyNumbers[2]);
+            Console.WriteLine(friends[0]);
+            Console.ReadLine();
+
+    }
+  }
+}
