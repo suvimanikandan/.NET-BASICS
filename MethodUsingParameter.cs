bb94994ac070527d0fcb76a038839d515f459cb4
swetha_suvi <80690771+suvimanikandan@users.noreply.github.com>
2023-11-20 16:03:35 +0530

    Create MethodUsingParameter.cs

---
+++ MethodUsingParameter.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    SayHi("swetha");
+   Console.ReadLine();
+   
+    }
+    
+    static void SayHi(string name)
+    {
+    Console.WriteLine(" Hello " + name);
+  }
+}
+}
