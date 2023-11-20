50787e4ed40814406d37fd71a028300fa4e8bfe7
swetha_suvi <80690771+suvimanikandan@users.noreply.github.com>
2023-11-20 16:42:44 +0530

    Create If.cs

---
+++ If.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    bool isMale = true;
+    bool isTall = true;
+    
+    if(isMale && isTall)
+    {
+    Console.WriteLine("you are a tall male");
+    }
+    else {
+    Console.WriteLine("You are either not male or not tall or both");
+    
+}
+Console.ReadLine();
+}
+}
+}
