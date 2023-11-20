c983487bbd5a95f10422aa536d8e77961ee48a62
swetha_suvi <80690771+suvimanikandan@users.noreply.github.com>
2023-11-20 16:01:38 +0530

    Create Methods.cs

---
+++ Methods.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    SayHi();
+   Console.ReadLine();
+   
+    }
+    
+    static void SayHi()
+    {
+    Console.WriteLine("Hello User");
+  }
+}
+}
