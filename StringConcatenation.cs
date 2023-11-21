08bb0fe29f23afebdd5f83be87e2d1903c21d9a6
swetha_suvi <80690771+suvimanikandan@users.noreply.github.com>
2023-11-21 13:06:54 +0530

    Create StringConcatenation.cs

---
+++ StringConcatenation.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string firstName = "John ";
+      string lastName = "jim";
+      string name = firstName + lastName;
+      Console.WriteLine(name);
+    }
+  }
+}
