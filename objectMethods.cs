3f43bc93abbfabf077edcfdd483dcf2cc19ba08d
swetha_suvi <80690771+suvimanikandan@users.noreply.github.com>
2023-11-21 14:35:22 +0530

    Create objectMethods.cs

---
+++ objectMethods.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace MyApplication
+{
+  class fruit {
+  string apple ="red";
+  string orange = "orange";
+  
+  public void fruitColor() {
+  Console.WriteLine("fruit colors");
+  }
+  
+  static void Main(string [] args){
+  fruit myObj=new fruit();
+  myObj.fruitColor();
+  }
+  }
+  }
+  
+   
+   
+   
+   
+   
