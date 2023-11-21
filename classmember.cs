e978ca2043df43ccd0ea11502c363f7379146b7c
swetha_suvi <80690771+suvimanikandan@users.noreply.github.com>
2023-11-21 14:17:16 +0530

    Create classmember.cs

---
+++ classmember.cs
@@ -0,0 +1,21 @@
+
+//class member
+using System;
+
+namespace MyApplication
+{
+  class Car
+  {
+  string color = "red";  //field
+  int maxspeed = 200;     //field
+  
+  static void Main(String[] args){
+  Car myObj=new Car(); //method
+  Console.WriteLine(myObj.color);
+  Console.WriteLine(myObj.maxspeed);
+   }
+   
+   }
+   
+   }
+   
