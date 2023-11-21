4832809e2e29358be3e05e2720825057bfeb2b1d
swetha_suvi <80690771+suvimanikandan@users.noreply.github.com>
2023-11-21 13:10:50 +0530

    Create Interpolation.cs

---
+++ Interpolation.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string firstName = "John ";
+      string lastName = "jim";
+      string name = $" my full name is: {firstName} {lastName}";
+      Console.WriteLine(name);
+    }
+  }
+}
